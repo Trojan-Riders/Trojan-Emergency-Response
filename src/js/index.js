b04ae298bcf343d32531afe3ba2e25ae01bde5b3
abirbhattacharya82 <abirbhattacharya82@gmail.com>
2022-09-10 09:48:56 +0530

    JS

--- src/js/index.js
+++ src/js/index.js
@@ -14,5 +14,44 @@ function toggle()
 }
 function submit()
 {
-    alert("Your Details are Submitted");
+    if(document.getElementById("home_location").value=="")
+    {
+        alert("Please enter a location");
+    }
+    else
+    {
+        document.getElementById("home_email_id").value="";
+        document.getElementById("home_phone").value="";
+        document.getElementById("home_location").value="";
+        alert("Your details are saved with us. We will contact you in case of any emergency in your area.")
+    }
+}
+function submit_beds()
+{
+    if(document.getElementById("beds_location").value=="" || document.getElementById("beds_phone").value=="")
+    {
+        alert("Please enter the mandatory fields");
+    }
+    else
+    {
+        document.getElementById("beds_name").value="";
+        document.getElementById("beds_phone").value="";
+        document.getElementById("beds_location").value="";
+        alert("We will be sending you details of bed availability in your provided phone number");
+    }
+}
+function submit_report()
+{
+    if(document.getElementById("report_location").value=="" || document.getElementById("report_phone").value=="" || document.getElementById("report_name").value=="" ||document.getElementById("report_disaster").value=="")
+    {
+        alert("Please enter the mandatory fields");
+    }
+    else
+    {
+        document.getElementById("report_name").value="";
+        document.getElementById("report_phone").value="";
+        document.getElementById("report_location").value="";
+        document.getElementById("report_disaster").value="";
+        alert("Thanks for the information we will be alerting the hospitals in the neighbouring area soon.");
+    }
 }
